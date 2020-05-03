665e5ce0bd7c988b74be2ab53043bd324f9d4c57
AToska21 <58345270+AToska21@users.noreply.github.com>
2020-05-03 23:18:56 +0200

    Create PluginAPI.php

---
+++ PluginAPI.php
@@ -0,0 +1,17 @@
+<?php
+/*
+__PocketMine Plugin__
+name=PluginTemplate
+version=1.0
+author=AToska21
+class=MyFirstPlugin
+apiversion=7
+*/
+class MyFirstPlugin implements Plugin{
+    public function __construct(ServerAPI $api, $server=false){
+    }
+    public function init(){
+    }
+    public function __destruct(){
+    }
+}
